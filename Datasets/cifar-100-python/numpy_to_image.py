c388e543658b5a75f2874ea638071493d1eeddba
Katie Bui <kgbui@uci.edu>
2017-05-11 11:36:37 -0700

    edited numpy to image
    gonna kill a bitch

--- Datasets/cifar-100-python/numpy_to_image.py
+++ Datasets/cifar-100-python/numpy_to_image.py
@@ -4,9 +4,17 @@ import numpy as np
 
 
 with open('trees_train_data_CIFAR_100.pickle', 'rb') as file:
+    imageCount = 1
     for data in pickle.load(file):
-        img = Image.fromarray(data, 'RGB')
-        img.save('my.png')
-        img.show()
-    
-    
+
+        img_R = data[0:1024].reshape((32, 32))
+        img_G = data[1024:2048].reshape((32, 32))
+        img_B = data[2048:3072].reshape((32, 32))
+        img = np.dstack((img_R, img_G, img_B))
+
+        # imsave(os.path.join(PIXELS_DIR, fname), img)
+
+        save_img = Image.fromarray(img, 'RGB')
+        save_img.save('tree' + str(imageCount) + '.png')
+        imageCount += 1
+        # img.show()
