be1b689f325ff05a3ba95efd0ea1650844eb778b
John Janecek <janecektyler@gmail.com>
2017-05-24 12:55:04 -0700

    renamed, used webcolors to create map this time

---
+++ AI/keymap.py
@@ -0,0 +1,213 @@
+ColorMap = {}
+
+
+ColorMap['#f4a460'] = 'BROWN'
+ColorMap['#a0522d'] = 'BROWN'
+ColorMap['#8b4513'] = 'BROWN'
+ColorMap['#d2b48c'] = 'BROWN'
+ColorMap['#a52a2a']= 'BROWN'
+ColorMap['#d2691e'] = 'BROWN'
+
+ColorMap['#7cfc00'] = 'GREEN'
+ColorMap['#00ff7f'] = 'GREEN'
+ColorMap['#008000'] = 'GREEN'
+ColorMap['#6b8e23'] = 'GREEN'
+ColorMap['#2e8b57'] = 'GREEN'
+ColorMap['#228b22'] = 'GREEN'
+ColorMap['#556b2f'] = 'GREEN'
+
+ColorMap['#808080'] = 'GRAY'
+ColorMap['#708090'] = 'GRAY'
+ColorMap['#a9a9a9'] = 'GRAY'
+ColorMap['#778899'] = 'GRAY'
+ColorMap['#696969'] = 'GRAY'
+
+ColorMap['#000000'] = 'BLACK'
+ColorMap['#b22222'] = 'BLACK'
+
+ColorMap['#00ff00'] = 'LIME'
+ColorMap['#90ee90'] = 'LIME'
+ColorMap['#8fbc8f'] = 'LIME'
+ColorMap['#adff2f'] = 'LIME'
+ColorMap['#7fff00'] = 'LIME'
+ColorMap['#3cb371'] = 'LIME'
+ColorMap['#808000'] = 'LIME'
+ColorMap['#98fb98'] = 'LIME'
+ColorMap['#32cd32'] = 'LIME'
+ColorMap['#006400'] = 'LIME'
+ColorMap['#9acd32'] = 'LIME'
+
+
+# BLUES
+'''
+ColorMap['#00ffff'] = 'BLUE'
+ColorMap['#00fa9a'] = 'BLUE'
+ColorMap['#4682b4'] = 'BLUE'
+ColorMap['#00008b'] = 'BLUE'
+ColorMap['#5f9ea0'] = 'BLUE'
+ColorMap['#2f4f4f'] = 'BLUE'
+ColorMap['#00ced1'] = 'BLUE'
+ColorMap['#6495ed'] = 'BLUE'
+ColorMap['#4169e1'] = 'BLUE'
+ColorMap['#40e0d0'] = 'BLUE'
+ColorMap['#1e90ff'] = 'BLUE'
+ColorMap['#7fffd4'] = 'BLUE'
+ColorMap['#0000ff'] = 'BLUE'
+ColorMap['#191970'] = 'BLUE'
+ColorMap['#008080'] = 'BLUE'
+ColorMap['#0000cd'] = 'BLUE'
+ColorMap['#000080'] = 'BLUE'
+ColorMap['#008b8b'] = 'BLUE'
+'''
+ColorMap['#00ffff'] = ''
+ColorMap['#00fa9a'] = ''
+ColorMap['#4682b4'] = ''
+ColorMap['#00008b'] = ''
+ColorMap['#5f9ea0'] = ''
+ColorMap['#2f4f4f'] = ''
+ColorMap['#00ced1'] = ''
+ColorMap['#6495ed'] = ''
+ColorMap['#4169e1'] = ''
+ColorMap['#40e0d0'] = ''
+ColorMap['#1e90ff'] = ''
+ColorMap['#7fffd4'] = ''
+ColorMap['#0000ff'] = ''
+ColorMap['#191970'] = ''
+ColorMap['#008080'] = ''
+ColorMap['#0000cd'] = ''
+ColorMap['#000080'] = ''
+ColorMap['#008b8b'] = ''
+
+
+
+# WHITES
+ColorMap['#fffacd'] = ''
+ColorMap['#d3d3d3'] = ''
+ColorMap['#ffe4c4'] = ''
+ColorMap['#e6e6fa'] = ''
+ColorMap['#dcdcdc'] = ''
+ColorMap['#fff0f5'] = ''
+ColorMap['#e0ffff'] = ''
+ColorMap['#87cefa'] = ''
+ColorMap['#ffdead'] = ''
+ColorMap['#ffdab9'] = ''
+ColorMap['#eee8aa'] = ''
+ColorMap['#faf0e6'] = ''
+ColorMap['#f5f5f5'] = ''
+ColorMap['#ffffe0'] = ''
+ColorMap['#f0ffff'] = ''
+ColorMap['#f0f8ff'] = ''
+ColorMap['#ffebcd'] = ''
+ColorMap['#fffafa'] = ''
+ColorMap['#ffe4b5'] = ''
+ColorMap['#fffff0'] = ''
+ColorMap['#fffaf0'] = ''
+ColorMap['#fff5ee'] = ''
+ColorMap['#ffe4e1'] = ''
+ColorMap['#ffffff'] = ''
+ColorMap['#b0e0e6'] = ''
+ColorMap['#f5fffa'] = ''
+ColorMap['#ffefd5'] = ''
+ColorMap['#f8f8ff'] = ''
+ColorMap['#faebd7'] = ''
+ColorMap['#f0fff0'] = ''
+ColorMap['#fdf5e6'] = ''
+ColorMap['#f5f5dc'] = ''
+ColorMap['#f5deb3'] = ''
+ColorMap['#fafad2'] = ''
+ColorMap['#fff8dc'] = ''
+
+ColorMap['#b8860b'] = 'ORANGE'
+ColorMap['#ff8c00'] = 'ORANGE'
+ColorMap['#ffa500'] = 'ORANGE'
+
+ColorMap['#7b68ee'] = 'PURPLE'
+ColorMap['#ff00ff'] = 'PURPLE'
+ColorMap['#8b008b'] = 'PURPLE'
+ColorMap['#dda0dd'] = 'PURPLE'
+ColorMap['#9400d3'] = 'PURPLE'
+ColorMap['#9370db'] = 'PURPLE'
+ColorMap['#9932cc'] = 'PURPLE'
+ColorMap['#4b0082'] = 'PURPLE'
+ColorMap['#da70d6'] = 'PURPLE'
+ColorMap['#c71585'] = 'PURPLE'
+ColorMap['#6a5acd'] = 'PURPLE'
+ColorMap['#ba55d3'] = 'PURPLE'
+ColorMap['#483d8b'] = 'PURPLE'
+ColorMap['#800080'] = 'PURPLE'
+ColorMap['#8a2be2'] = 'PURPLE'
+
+
+
+ColorMap['#ee82ee'] = 'PINK'
+ColorMap['#ff1493'] = 'PINK'
+ColorMap['#d8bfd8'] = 'PINK'
+ColorMap['#ff69b4'] = 'PINK'
+ColorMap['#bc8f8f'] = 'PINK'
+ColorMap['#db7093'] = 'PINK'
+ColorMap['#ffb6c1'] = 'PINK'
+ColorMap['#f08080'] = 'PINK'
+ColorMap['#ffc0cb'] = 'PINK'
+
+
+# LIGHT_BLUE
+ColorMap['#afeeee'] = ''
+ColorMap['#add8e6'] = ''
+ColorMap['#66cdaa'] = ''
+ColorMap['#48d1cc'] = ''
+ColorMap['#00bfff'] = ''
+ColorMap['#20b2aa'] = ''
+ColorMap['#87ceeb'] = ''
+ColorMap['#b0c4de'] = ''
+
+
+
+ColorMap['#800000'] = 'RED'
+ColorMap['#fa8072'] = 'RED'
+ColorMap['#8b0000'] = 'RED'
+ColorMap['#ff6347'] = 'RED'
+ColorMap['#cd5c5c'] = 'RED'
+ColorMap['#dc143c'] = 'RED'
+ColorMap['#ff0000'] = 'RED'
+
+ColorMap['#daa520'] = 'YELLOW'
+ColorMap['#f0e68c'] = 'YELLOW'
+ColorMap['#ffd700'] = 'YELLOW'
+ColorMap['#bdb76b'] = 'YELLOW'
+ColorMap['#ffff00'] = 'YELLOW'
+
+ColorMap['#ff7f50'] = 'ORANGE'
+ColorMap['#e9967a'] = 'ORANGE'
+ColorMap['#ffa07a'] = 'ORANGE'
+ColorMap['#deb887'] = 'ORANGE'
+ColorMap['#cd853f'] = 'ORANGE'
+ColorMap['#ff4500'] = 'ORANGE'
+
+
+# SILVER
+ColorMap['#c0c0c0'] = ''
+
+
+
+# ADDITIONALS
+
+# Dark browns
+ColorMap['#996633'] = 'BROWN'
+ColorMap['#734d26'] = 'BROWN'
+ColorMap['#ac7339'] = 'BROWN'
+#ColorMap['#996633'] = 'BROWN'
+
+# GREENS
+ColorMap['#004d00'] = 'GREEN'
+ColorMap['#003300'] = 'GREEN'
+ColorMap['#006600'] = 'GREEN'
+
+# OTHER GREENS
+ColorMap['#206040'] = 'GREEN'
+ColorMap['#194d33'] = 'GREEN'
+ColorMap['#2d8659'] = 'GREEN'
+
+
+
+
+
